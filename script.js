7031077238baf05ede15d24a9f316c262db78964
Ujala Singh <100023575+UjalaSingh@users.noreply.github.com>
2023-06-22 18:36:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const quoteText = document.querySelector(".quote");
+const quoteBtn = document.querySelector("button");
+const authorName = document.querySelector(".name");
+const speechBtn = document.querySelector(".speech");
+const copyBtn = document.querySelector(".copy");
+const twitterBtn = document.querySelector(".twitter");
+const synth = speechSynthesis;
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("https://api.quotable.io/random")
+    .then((response) => response.json())
+    .then((result) => {
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
+    })
+    .catch((error) => {
+      console.log(error);
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
+    });
+}
+
+speechBtn.addEventListener("click", () => {
+  if (!quoteBtn.classList.contains("loading")) {
+    let utterance = new SpeechSynthesisUtterance(
+      `${quoteText.innerText} by ${authorName.innerText}`
+    );
+    synth.speak(utterance);
+    setInterval(() => {
+      !synth.speaking
+        ? speechBtn.classList.remove("active")
+        : speechBtn.classList.add("active");
+    }, 10);
+  }
+});
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click", () => {
+  let tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    `${quoteText.innerText} - ${authorName.innerText}`
+  )}`;
+  window.open(tweetUrl, "_blank");
+});
+
+quoteBtn.addEventListener("click", randomQuote);
+
+randomQuote(); // Call randomQuote() initially to load a quote when the page loads
